548e4476d236ebf57a53edfb901af7bf7a15dc3c
leeyh <leeyh@isung.com>
2019-12-03 02:06:18 +0900

    init

---
+++ Pods/Target Support Files/Pods-Idus_Test_App-Idus_Test_AppUITests/Pods-Idus_Test_App-Idus_Test_AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Idus_Test_App_Idus_Test_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Idus_Test_App_Idus_Test_AppUITests
+@end
